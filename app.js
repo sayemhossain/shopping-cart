396bde3a8cb20a81e0f27ba29bae55841838391d
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-13 00:24:56 +0600

    new code added

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-function updateCaseNumber(isAdd) {
-  const caseInput = document.getElementById("case-number");
-  const caseNumber = caseInput.value;
-  if (isAdd == true) {
-    caseInput.value = parseInt(caseNumber) + 1;
-  } else {
-    caseInput.value = parseInt(caseNumber) - 1;
+function updateCaseNumber(quantity, isIncreasing, priceId, price) {
+  const input = document.getElementById(quantity);
+  var totalNumber = input.value;
+  if (isIncreasing == true) {
+    totalNumber = parseInt(totalNumber) + 1;
+  } else if (totalNumber > 0) {
+    totalNumber = parseInt(totalNumber) - 1;
   }
+  input.value = totalNumber;
+  //   this is for showing price
+  const addPrice = document.getElementById(priceId);
+  addPrice.innerText = totalNumber * price;
 }
 document.getElementById("case-plus").addEventListener("click", function () {
-  updateCaseNumber(true);
+  updateCaseNumber("case-number", true, "case-total", 59);
 });
 document.getElementById("case-minus").addEventListener("click", function () {
-  //   const caseInput = document.getElementById("case-number");
-  //   const caseNumber = caseInput.value;
-  //   caseInput.value = parseInt(caseNumber) - 1;
-  updateCaseNumber(false);
+  updateCaseNumber("case-number", false, "case-total", 59);
+});
+
+// this is for phone quantity
+document.getElementById("phone-plus").addEventListener("click", function () {
+  updateCaseNumber("phone-number", true, "phone-total", 1219);
+});
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updateCaseNumber("phone-number", false, "phone-total", 1219);
 });
