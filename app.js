50e6e2ea7e5347c894895e47125d159581f2f830
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-12 23:51:59 +0600

    added new code

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('I am in a separate file')
+function updateCaseNumber(isAdd) {
+  const caseInput = document.getElementById("case-number");
+  const caseNumber = caseInput.value;
+  if (isAdd == true) {
+    caseInput.value = parseInt(caseNumber) + 1;
+  } else {
+    caseInput.value = parseInt(caseNumber) - 1;
+  }
+}
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateCaseNumber(true);
+});
+document.getElementById("case-minus").addEventListener("click", function () {
+  //   const caseInput = document.getElementById("case-number");
+  //   const caseNumber = caseInput.value;
+  //   caseInput.value = parseInt(caseNumber) - 1;
+  updateCaseNumber(false);
+});
